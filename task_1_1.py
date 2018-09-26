66e0e94a6957a47142212419352551cad47c5b73
Paladugu33 <38241488+Paladugu33@users.noreply.github.com>
2018-09-26 18:32:58 +0530

    Add files via upload

---
+++ task_1_1.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 12 17:18:44 2018
+
+@author: hmohan
+"""
+
+import mysql.connector
+import xlrd
+import os
+import pandas
+from datetime import datetime
+from sqlalchemy import create_engine
+
+
+
+try: 
+    xl= pandas.ExcelFile('/home/hmohan/Desktop/inspections .xlsx')
+#xl.sheet_names
+    fd1 = xl.parse('inspections')
+
+#print (fd1.head())
+
+    engine = create_engine("mysql+mysqlconnector://root:admin@localhost/mydb")
+
+    con = engine.connect()
+    fd1.to_sql(name='INSPECTIONS',con=con,if_exists='append')
+    con.close()
+    print ('table apdded')
+except Exception:
+    print (e)
+
+
+xl= pandas.ExcelFile('/home/hmohan/Desktop/violations.xlsx')
+#xl.sheet_names
+fd1 = xl.parse('violations')
+
+#print (fd1.head())
+
+engine = create_engine("mysql+mysqlconnector://root:admin@localhost/mydb")
+
+con = engine.connect()
+fd1.to_sql(name='VIOLATIONS',con=con,if_exists='append')
+con.close()
+print ('table added')
